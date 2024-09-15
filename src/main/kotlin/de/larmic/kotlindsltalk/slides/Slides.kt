1f4acac795538926f70456397a1e4f1172880c14
larmic <mail@larmic.de>
2024-09-15 09:37:28 +0200

    add bad example

--- src/main/kotlin/de/larmic/kotlindsltalk/slides/Slides.kt
+++ src/main/kotlin/de/larmic/kotlindsltalk/slides/Slides.kt
@@ -26,3 +26,28 @@ val p = presentation {
         }
     }
 }
+
+fun bad() {
+    val company = Company()
+    company.name = "Team Neusta"
+    company.city = "Bremen"
+    company.employees = 1400
+
+    val social = Social()
+    social.linkedIn = "https://www.linkedin.com/in/lars-michaelis-6778761b3/"
+    social.twitter = "https://twitter.com/larmicDE"
+
+    val speaker = Speaker()
+    speaker.name = "Lars Michaelis"
+    speaker.email = "l.michaelis@neusta.de"
+    speaker.jobTitle = "Solution Architect, Technical Coach"
+    speaker.social = social
+    speaker.company = company
+
+    val presentation = Presentation()
+    presentation.title = "Eine Einführung in Kotlin DSL"
+    presentation.conference = KKON
+    presentation.date = 16 SEPTEMBER 2024
+    presentation.github = "https://github.com/larmic/kotlin-dsl-talk"
+    presentation.speaker = speaker
+}
